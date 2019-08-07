db7f1521832d84d0de7bc7a43aa9326216a80026
m_623914 <thirukkumaran.natarajan@cognizant.com>
2019-08-07 14:58:25 +0800

    echo host & port details for troubleshooting

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ url="$(echo ${HTTP_PROXY/$proto/})"
 userpass="$(echo $url | grep @ | cut -d@ -f1)"
 PROXY_PASS="$(echo $userpass | grep : | cut -d: -f2)"
 
+
+
 if [ -n "$PROXY_PASS" ]; then
   PROXY_USER="$(echo $userpass | grep : | cut -d: -f1)"
 else
@@ -20,5 +22,11 @@ hostport="$(echo ${url/$userpass/} | cut -d@ -f2)"
 PROXY_HOST="$(echo $hostport |  cut -d: -f1)"
 PROXY_PORT="$(echo $hostport |  cut -d: -f2)"
 
+echo $LOCAL_PORT
+echo $PROXY_HOST
+echo $PROXY_USER
+echo $PROXY_PASS
+echo $DEST_HOST
+echo $DEST_PORT
 
 /bin/socat -d -d -d TCP-L:$LOCAL_PORT,fork,reuseaddr PROXY:$PROXY_HOST:$DEST_HOST:$DEST_PORT,proxyport=$PROXY_PORT,proxyauth=$PROXY_USER:$PROXY_PASS
